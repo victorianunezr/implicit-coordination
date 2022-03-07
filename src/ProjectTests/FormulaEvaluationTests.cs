6e063ee56ba29ba8e4e79255977986c91beed448
yvictorianunezr <victorianunezr96@gmail.com>
2022-03-07 17:59:28 +0100

    Second order knowledge UT passing

--- src/ProjectTests/FormulaEvaluationTests.cs
+++ src/ProjectTests/FormulaEvaluationTests.cs
@@ -34,7 +34,7 @@ namespace DEL.Tests
         }
 
         [Test]
-        public void PropositionalLogic_EvaluateInWorld()
+        public void PropositionalLogic()
         {
             // Evaluating formulas in a world. The setup is:
             // p = false (idx = 0), q = true (idx = 1), r = true (idx = 1)
@@ -69,7 +69,7 @@ namespace DEL.Tests
         }
 
         [Test]
-        public void EpistemicLogic_FirstOrderKnowledge_EvaluateInWorld()
+        public void EpistemicLogic_FirstOrderKnowledge()
         {
             // Setup state with one agent: State S = {W, R}, W = {w, u, v}, R = {(w, u), (w, v)}
             // Propositions p (LSB),q ,r (MSB). Valuations:
@@ -89,8 +89,6 @@ namespace DEL.Tests
             var f5 = Formula.Knows(a, Formula.And(atomP, atomQ));
             var f6 = Formula.Knows(a, Formula.Or(atomP, atomQ));
 
-
-
             // Assert
             Assert.IsFalse(f1.Evaluate(s, w));
             Assert.IsTrue(f1.Evaluate(s, u));
@@ -103,6 +101,49 @@ namespace DEL.Tests
             Assert.IsFalse(f5.Evaluate(s, w));
             Assert.IsTrue(f5.Evaluate(s, u));
             Assert.IsTrue(f6.Evaluate(s, w));
+        }
+
+        [Test]
+        public void EpistemicLogic_SecondOrderKnowledge()
+        {
+            // Setup state with agents a and b: State S = {W, R},
+            // W = {w, u, v, t},
+            // R[a] = {(w, u), (w, v), (t, w)}
+            // R[b] = {(u, v), (w, v)}
+            // Propositions p (LSB),q ,r (MSB). Valuations:
+            // w = 111, u = 011, v = 110, t = 001
+
+            var t = new World(0b100);
+            Agent a = new Agent();
+            Agent b = new Agent();
+
+            AccessibilityRelation r = new AccessibilityRelation(new HashSet<Agent> { a, b });
+            r.AddEdge(a, (w, v));
+            r.AddEdge(a, (w, u));
+            r.AddEdge(a, (w, t));
+            r.AddEdge(b, (w, v));
+            r.AddEdge(b, (u, v));
+            State s = new State(new HashSet<World> { w, u, v, t }, null, r);
+
+            // Assert
+            var f1 = Formula.Knows(a, Formula.Knows(b, atomQ));
+            Assert.IsTrue(f1.Evaluate(s, v));
+            Assert.IsFalse(f1.Evaluate(s, w));
+            Assert.IsTrue(f1.Evaluate(s, u));
+            Assert.IsFalse(f1.Evaluate(s, t));
+
+            Assert.IsTrue(Formula.Knows(b, atomR).Evaluate(s, w));
+            Assert.IsTrue(Formula.Knows(b, atomR).Evaluate(s, t));
+            var f2 = Formula.Knows(a, Formula.Knows(b, atomR));
+            Assert.IsTrue(f2.Evaluate(s, t));
+            Assert.IsFalse(f2.Evaluate(s, w));
+            Assert.IsFalse(f2.Evaluate(s, u));
+
+            var f3 = Formula.Knows(b, Formula.Knows(a, atomP));
+            Assert.IsFalse(f3.Evaluate(s, w));
+            Assert.IsFalse(f3.Evaluate(s, u));
+            Assert.IsFalse(f3.Evaluate(s, t));
+            Assert.IsFalse(f3.Evaluate(s, v));
         }
     }
 }
