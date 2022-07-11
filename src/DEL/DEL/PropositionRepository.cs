1d4d1a68b4c4544e8889edcb2d98deef43188729
yvictorianunezr <victorianunezr96@gmail.com>
2022-07-11 10:34:49 +0200

    add proposition repository

---
+++ src/DEL/DEL/PropositionRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplicitCoordination.DEL
+{
+    public static class PropositionRepository
+    {
+        public static Dictionary<string, Proposition> Propositions;
+        
+        public static void Add(Proposition p)
+        {
+            Propositions.Add(p.name, p);
+        }
+
+        public static Proposition Get(string name)
+        {
+            Proposition p;
+            if (Propositions.TryGetValue(name, out p))
+            {
+                return p;
+            }
+            throw new Exception("Proposition not found");
+        }
+    }
+}
